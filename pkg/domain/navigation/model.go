a79cb4d78376787b3f19112039f23255a984f0f8
Rui Ma <535657150@qq.com>
2023-11-28 13:17:02 +0800

    优化菜单图标显示方式

--- pkg/domain/navigation/model.go
+++ pkg/domain/navigation/model.go
@@ -5,14 +5,15 @@ import (
 )
 
 type Navigation struct {
-	ID     uint   `json:"id" gorm:"autoIncrement;primaryKey"`
-	Title  string `json:"title" gorm:"not null"`
-	Href   string `json:"href"`
-	Logo   string `json:"logo"`
-	Desc   string `json:"desc"`
-	Parent uint   `json:"parent" default:"0"` // 0为parent
-	Order  int    `json:"order"`
-	Unique string `json:"-" gorm:"unique;not null"`
+	ID       uint   `json:"id" gorm:"autoIncrement;primaryKey"`
+	Title    string `json:"title" gorm:"not null"`
+	Href     string `json:"href"`
+	Logo     string `json:"logo"`
+	LogoData string `json:"logoData"`
+	Desc     string `json:"desc"`
+	Parent   uint   `json:"parent" default:"0"` // 0为parent
+	Order    int    `json:"order"`
+	Unique   string `json:"-" gorm:"unique;not null"`
 
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
--- pkg/domain/navigation/service.go
+++ pkg/domain/navigation/service.go
@@ -27,13 +27,17 @@ func (s *Service) ListNavigation() ([]*Navigation, error) {
 		return nil, err
 	}
 	for _, item := range res {
-		if len(item.Logo) == 0 {
+		if len(item.Logo) == 0 || len(item.LogoData) > 0 {
 			continue
 		}
+		logrus.Debugf("imgutil.ImgLinkToBase64: %s", item.Logo)
 		data, err := imgutil.ImgLinkToBase64(item.Logo)
 		if err == nil {
-			item.Logo = data
+			item.LogoData = data
+			s.UpdateNavigation(item)
 		} else {
+			item.LogoData = item.Logo
+			s.UpdateNavigation(item)
 			logrus.Errorf("imgutil.ImgLinkToBase64 err: %v", err)
 		}
 	}
@@ -52,6 +56,16 @@ func (s *Service) AddNavigation(navigation *Navigation) error {
 	//	return errors.New("菜单名称重复")
 	//}
 
+	if len(navigation.Logo) > 0 {
+		data, err := imgutil.ImgLinkToBase64(navigation.Logo)
+		if err == nil {
+			navigation.LogoData = data
+		} else {
+			navigation.LogoData = navigation.Logo
+			logrus.Errorf("imgutil.ImgLinkToBase64 err: %v", err)
+		}
+	}
+
 	navigation.Unique = fmt.Sprintf("%s-%d", navigation.Title, navigation.Parent)
 	return s.persistence.Insert(navigation)
 }
@@ -65,6 +79,16 @@ func (s *Service) UpdateNavigation(navigation *Navigation) error {
 	//if err == nil && res.Parent == navigation.Parent && (res.Title != navigation.Title || res.Parent != navigation.Parent) {
 	//	return errors.New("菜单名称重复")
 	//}
+
+	if len(navigation.Logo) > 0 {
+		data, err := imgutil.ImgLinkToBase64(navigation.Logo)
+		if err == nil {
+			navigation.LogoData = data
+		} else {
+			navigation.LogoData = navigation.Logo
+			logrus.Errorf("imgutil.ImgLinkToBase64 err: %v", err)
+		}
+	}
 
 	navigation.Unique = fmt.Sprintf("%s-%d", navigation.Title, navigation.Parent)
 	return s.persistence.Update(&Navigation{ID: navigation.ID}, structutil.Struct2Map(navigation))
